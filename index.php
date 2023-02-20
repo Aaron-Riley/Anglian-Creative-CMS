aa5792fa640ed7f069070dd5d9d5ac402c733edc
AaronG19 <72134243+Aaron-Riley@users.noreply.github.com>
2023-02-20 10:37:40 +0000

    Update

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once(__DIR__.'/includes/boot.include.php');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c8cb604adaaa458c15aee39fe63ca5205c2987a3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c8cb604adaaa458c15aee39fe63ca5205c2987a3',
+            'dev_requirement' => false,
+        ),
+        'smarty/smarty' => array(
+            'pretty_version' => 'v4.3.0',
+            'version' => '4.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smarty/smarty',
+            'aliases' => array(),
+            'reference' => 'c02e9e135ea719b91f457a0072748ded0e852e7d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
